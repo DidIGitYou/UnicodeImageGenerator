0e483e0378d4389f0ac3697e95a3f1394c20d125
DidIGitYou <64001632+DidIGitYou@users.noreply.github.com>
2021-07-13 09:43:58 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+using System.Threading;
+using System.Drawing;
+
+namespace DrawImage
+{
+    class Program
+    {
+        static void Main()
+        {
+            Image Picture = Image.FromFile(""); //Put Your picture here
+            Console.SetBufferSize((Picture.Width * 0x2), (Picture.Height * 0x2));
+            Console.WindowWidth = 180;
+            Console.WindowHeight = 61;
+
+            FrameDimension Dimension = new FrameDimension(Picture.FrameDimensionsList[0x0]);
+            int FrameCount = Picture.GetFrameCount(Dimension);
+            int Left = Console.WindowLeft, Top = Console.WindowTop;
+            char[] Chars = { '#', '#', '@', '%', '=', '+', '*', ':', '-', '.', ' ' };
+            Picture.SelectActiveFrame(Dimension, 0x0);
+            for (int i = 0x0; i < Picture.Height; i++)
+            {
+                for (int x = 0x0; x < Picture.Width; x++)
+                {
+                    Color Color = ((Bitmap)Picture).GetPixel(x, i);
+                    int Gray = (Color.R + Color.G + Color.B) / 0x3;
+                    int Index = (Gray * (Chars.Length - 0x1)) / 0xFF;
+                    Console.Write(Chars[Index]);
+                }
+                Console.Write('\n');
+                Thread.Sleep(75);
+            }
+            Console.SetCursorPosition(Left, Top);
+            Console.Read();
+        }
+    }
+}
